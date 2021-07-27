23d810e1acc341e096cb8b7eb2f9c0878a440e1f
Breck Cowan <breckcowan@gmail.com>
2021-07-27 15:26:50 -0500

    updating information within the site with personal info

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 
-const homeStartingContent = "This is the beginings of breck cowans professional website(add some pictures and such)";
+const homeStartingContent = "Hello World!  Welcome to my dive into web developement.  ";
 const aboutContent = "This is going to be my content area(attach git hub once it is filled, put in projects)";
-const contactContent = "This is going to be the area for contact information and such(connect mailchimp api, weather api for san antonio, connect to github, linkedin)";
+const contactContent = "If you would like to contact me, please enter your informtion in the form below and I will return your message at the earliest opportunity.  Thank you!";
 
 const app = express();
 
@@ -15,6 +15,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(bodyParser.json());
 
 let posts = [];
 
@@ -32,6 +33,105 @@ app.get("/about", function(req, res){
 app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
+
+
+/* testing adding mailchimp API
+
+
+
+
+app.post("/", function(req, res){
+
+//creating variable to collect inputs from client side((div name="input"))
+
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+//creating data object to be able to merge https/server
+
+    const data = {
+      members: [
+        {
+          email_address: email,
+          status: "subscribed",
+          merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName
+          }
+        }
+      ]
+    };
+
+//creates the json Data as a string to send to mailchimp
+
+    const jsonData = JSON.stringify(data);
+
+//creats the constant for the url connected to mailchimp
+
+    const url = "https://us6.api.mailchimp.com/3.0/lists/6042b002b1"
+
+//creating options which are going to be a javascript object
+//most important option is the method
+//now that the methods and the API Key is in place, this gives the request below a route
+
+    const options = {
+      method: "POST",
+      auth: "breck1:d4fc8374241b0fc06d3a380ca97f4b70-us6"
+    }
+
+//creating the request
+//turned https request into a constant to handle future requests
+
+    const request = https.request(url, options, function(response) {
+
+//setting up status codes
+
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+
+//creating the response in a parsed JSON data object
+
+      response.on("data", function(data) {
+        console.log(JSON.parse(data));
+      });
+    });
+
+//this writes the jsonData and sends to mailchimp
+
+    request.write(jsonData);
+
+//this ends the request
+    request.end();
+
+});
+
+//adding a post request for the failure route
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+//naming the port that the server is running on
+//changed from port 3000 to process.env.PORT || 3000 to connect web app with Heroku or locally on port 3000
+
+
+//API Key
+//d4fc8374241b0fc06d3a380ca97f4b70-us6
+//List Id
+//6042b002b1
+
+
+
+
+*/
+
+
+
+
 
 app.get("/compose", function(req, res){
   res.render("compose");
